1b9fa98d7c09ab6357f74c4ae8683de8c3811985
Devin Walters <devinw@gmail.com>
2013-08-23 16:28:23 -0500

    Bump version and add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.pbqr/sente "0.1.0"
-  :description ""
+(defproject org.pbqr/sente "0.4.0"
+  :description "Useful tools for development"
   :url "https://github.com/devn/sente"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
